fd6da9c5aebca6898fc76e71582424bfe2077b67
Ron Rave <ronrave12@gmail.com>
2024-01-27 22:50:50 +0200

    Use ChangeStream

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -13,13 +14,45 @@ func main() {
 		panic(err)
 	}
 
-	result, err := mongoService.DetailedConsume("db1", "c1", 15*time.Second)
+	mongoService.Purge("db1", "c1")
+	fmt.Println("purged")
+	err = mongoService.Populate("db1", "c1")
 	if err != nil {
 		panic(err)
 	}
-	for _, doc := range result {
-		fmt.Println(string(doc.Body))
-		fmt.Println(doc.Timestamp)
-		fmt.Println()
+
+	resultChan := make(chan any)
+	go func() {
+		fmt.Println(time.Now())
+		detailedMessages, err := mongoService.DetailedConsume("db1", "c1", 5*time.Second)
+		if err != nil {
+			resultChan <- err
+			return
+		}
+		resultChan <- detailedMessages
+	}()
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("purged again")
+	fmt.Println(time.Now())
+	mongoService.Purge("db1", "c1")
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("re-populate")
+	mongoService.PupolateBen("db1", "c1")
+
+	result := <-resultChan
+	switch v := result.(type) {
+	case error:
+		panic(v)
+	case []*mongo.DetailedMessage:
+		for _, doc := range v {
+			// fmt.Println(string(doc.Body))
+			fmt.Println(doc.Timestamp)
+			fmt.Println()
+		}
+	default:
+		panic(errors.New("unknown type"))
 	}
+	fmt.Println(time.Now())
 }
--- mongo/consumer.go
+++ mongo/consumer.go
@@ -5,12 +5,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 const (
@@ -33,13 +35,24 @@ type DetailedMessage struct {
 	Timestamp time.Time
 }
 
-type Document struct {
+type changeEvent struct {
+	id  string
+	doc bson.M
+}
+
+type document struct {
 	Doc       bson.M
 	Timestamp time.Time
 }
 
 func NewMongoService(hostname string, port int, username, password string) (*MongoService, error) {
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s:%d", username, password, hostname, port)))
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%d", hostname, port)))
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(ctx, readpref.PrimaryPreferred())
 	if err != nil {
 		return nil, err
 	}
@@ -64,22 +77,27 @@ func (m *MongoService) DetailedConsume(databaseName, collectionName string, time
 		return nil, err
 	}
 
-	docChan := make(chan bson.M)
+	docChan := make(chan *changeEvent)
+	errChan := make(chan error)
 
-	go watchForChanges(docChan, collection)
+	go watchForChanges(docChan, errChan, collection)
 
 	watch := true
 	for watch {
+		timeoutChan := time.After(timeout)
+		fmt.Println("restart timer")
 		select {
-		case <-time.After(timeout):
+		case <-timeoutChan:
 			fmt.Println("timeout reached")
 			watch = false
 		case doc := <-docChan:
-			id, err := getDocId(doc)
-			if err != nil {
-				return nil, err
+			if doc.doc == nil {
+				delete(docs, doc.id)
+			} else {
+				docs[doc.id] = &document{Doc: doc.doc, Timestamp: time.Now()}
 			}
-			docs[id] = &Document{Doc: doc, Timestamp: time.Now()}
+		case err := <-errChan:
+			return nil, err
 		}
 	}
 
@@ -93,15 +111,15 @@ func (m *MongoService) DetailedConsume(databaseName, collectionName string, time
 	return
 }
 
-func mapDocs(docs []bson.M) (map[string]*Document, error) {
-	docsMap := make(map[string]*Document)
+func mapDocs(docs []bson.M) (map[string]*document, error) {
+	docsMap := make(map[string]*document)
 
 	for _, doc := range docs {
 		id, err := getDocId(doc)
 		if err != nil {
 			return nil, err
 		}
-		docsMap[id] = &Document{Doc: doc, Timestamp: time.Now()}
+		docsMap[id] = &document{Doc: doc, Timestamp: time.Now()}
 	}
 
 	return docsMap, nil
@@ -137,81 +155,95 @@ func (m *MongoService) fetchAll(ctx context.Context, collection *mongo.Collectio
 	return
 }
 
-// func findChanges(last, current map[string]bson.M) bool {
-// 	if len(current) != len(last) {
-// 		return true
-// 	}
-
-// 	for key, currentValue := range current {
-// 		lastValue, ok := last[key]
-// 		if !ok || !reflect.DeepEqual(currentValue, lastValue) {
-// 			return true
-// 		}
-// 	}
-// 	return false
-// }
-
-func watchForChanges(docChan chan bson.M, collection *mongo.Collection) (err error) {
+func watchForChanges(docChan chan *changeEvent, errChan chan error, collection *mongo.Collection) {
 	ctx := context.Background()
 	changeStream, err := collection.Watch(ctx, mongo.Pipeline{})
 	if err != nil {
-		return err
+		errChan <- err
+		return
 	}
 	defer changeStream.Close(ctx)
 
 	for changeStream.Next(ctx) {
-		var doc bson.M
-		if err = changeStream.Decode(&doc); err != nil {
-			return err
+		var event bson.M
+		if err = changeStream.Decode(&event); err != nil {
+			errChan <- err
+			return
 		}
-		docChan <- doc
+		id, ok := ((event["documentKey"]).(bson.M)["_id"]).(primitive.ObjectID)
+		if !ok {
+			errChan <- errors.New("could not get '_id' of change event")
+		}
+
+		doc, ok := event["fullDocument"]
+		if ok {
+			docChan <- &changeEvent{
+				id:  id.Hex(),
+				doc: doc.(bson.M),
+			}
+		} else {
+			docChan <- &changeEvent{
+				id:  id.Hex(),
+				doc: nil,
+			}
+		}
 	}
+}
+
+func (m *MongoService) Populate(databaseName, collectionName string) (err error) {
+	// Set up MongoDB connection parameters
+
+	// Use a database and collection
+	database := m.client.Database(databaseName)
+	collection := database.Collection(collectionName)
+
+	// Create the database and collection if they don't exist
+	// err := database.CreateCollection(context.Background(), collectionName)
+	// if err != nil {
+	// 	log.Printf("Database or collection creation error: %v", err)
+	// } else {
+	// 	fmt.Printf("Database '%s' and collection '%s' created\n", databaseName, collectionName)
+	// }
+
+	// Insert documents
+	documents := []interface{}{
+		map[string]interface{}{"name": "John", "age": 30},
+		map[string]interface{}{"name": "Jane", "age": 25},
+		map[string]interface{}{"name": "Bob", "age": 35},
+		// map[string]interface{}{"name": "Ben", "age": age},
+	}
+
+	insertResult, err := collection.InsertMany(context.Background(), documents)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Inserted %v documents\n", len(insertResult.InsertedIDs))
 	return nil
 }
 
-// func populate() {
-// 	// Set up MongoDB connection parameters
-// 	clientOptions := options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@localhost:27017", username, password))
-// 	client, err := mongo.Connect(context.Background(), clientOptions)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer func() {
-// 		if err := client.Disconnect(context.Background()); err != nil {
-// 			log.Fatal(err)
-// 		}
-// 	}()
+func (m *MongoService) PupolateBen(databaseName, collectionName string) {
+	// Set up MongoDB connection parameters
 
-// 	// Check the connection
-// 	err = client.Ping(context.Background(), nil)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Println("Connected to MongoDB!")
+	collection := m.client.Database(databaseName).Collection(collectionName)
 
-// 	// Use a database and collection
-// 	database := client.Database(databaseName)
-// 	collection := database.Collection(collectionName)
+	// Insert documents
+	documents := []interface{}{
+		map[string]interface{}{"name": "Ben", "age": 23},
+	}
 
-// 	// Create the database and collection if they don't exist
-// 	err = database.CreateCollection(context.Background(), collectionName)
-// 	if err != nil {
-// 		log.Printf("Database or collection creation error: %v", err)
-// 	} else {
-// 		fmt.Printf("Database '%s' and collection '%s' created\n", databaseName, collectionName)
-// 	}
+	insertResult, err := collection.InsertMany(context.Background(), documents)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-// 	// Insert documents
-// 	documents := []interface{}{
-// 		map[string]interface{}{"name": "John", "age": 30},
-// 		map[string]interface{}{"name": "Jane", "age": 25},
-// 		map[string]interface{}{"name": "Bob", "age": 35},
-// 	}
+	fmt.Printf("Inserted %v documents\n", len(insertResult.InsertedIDs))
+}
 
-// 	insertResult, err := collection.InsertMany(context.Background(), documents)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	fmt.Printf("Inserted %v documents\n", len(insertResult.InsertedIDs))
-// }
+func (m *MongoService) Purge(databaseName, collectionName string) {
+	collection := m.client.Database(databaseName).Collection(collectionName)
+	_, err := collection.DeleteMany(context.Background(), map[string]interface{}{})
+	if err != nil {
+		panic(err)
+	}
+}
